6c4dddd293ccdd7b7f54b632f468ea74f5c99c01
markxoe <mark@oude-elberink.de>
2021-05-13 10:32:20 +0200

    :recycle: NEW: Readable code :tada:

--- app/render.js
+++ app/render.js
@@ -1,13 +1,106 @@
 const main = document.getElementById("main");
 const crypto = require("crypto");
 
-const electronStore = require("electron-store");
 const electron = require("electron").remote;
 const ipcRender = require("electron").ipcRenderer;
 
 const currentWindow = electron.getCurrentWindow();
 
 const tabs = require("./data");
+
+//#region Rendering functions
+const reRenderButtons = () => {
+  main.innerHTML = "";
+  tabs.getButtonsFromTab().forEach((link) => {
+    // Generate Background and text color
+    const colors = getTextAndBackgroundColor(link);
+
+    // Generate root element
+    const rootEl = document.createElement("div");
+    rootEl.classList.add("button-outer");
+
+    // Generate inner button element
+    const buttonEl = document.createElement("button");
+    buttonEl.classList.add("button-inner");
+    // Set innerText to rewritten link and attribute "link" to link
+    buttonEl.innerText = genInner(link);
+    buttonEl.setAttribute("link", link);
+    // Add event listener for onClick event
+    buttonEl.addEventListener("click", function () {
+      const link = this.getAttribute("link"); // Get Link and open
+      electron.shell.openExternal(link);
+    });
+
+    // Generate inner remove button element
+    const buttonRemoveEl = document.createElement("button");
+    buttonRemoveEl.classList.add("button-remove");
+    buttonRemoveEl.innerHTML = "&times;"; // Inner text to x-like symbol
+    buttonRemoveEl.setAttribute("link", link); // Set attribute "link" to link
+    // Adding Event Listener
+    buttonRemoveEl.addEventListener("click", function () {
+      const link = this.getAttribute("link"); // Get link
+      tabs.removeButtonFromTab(link); // Remove it
+    });
+
+    // Set Styling
+    rootEl.style.backgroundColor = colors.background;
+    buttonEl.style.color = colors.textcolor;
+
+    // Add inner elements to root
+    rootEl.appendChild(buttonEl);
+    rootEl.appendChild(buttonRemoveEl);
+    // Add root element to DOM
+    main.appendChild(rootEl);
+  });
+};
+
+const renderTabSelector = () => {
+  const tabselect = document.getElementById("tabselect");
+  // Empty
+  tabselect.innerHTML = "";
+  // Add Buttons
+  for (let tab of tabs.getTabNamesAndIndexes()) {
+    const _tempEl = document.createElement("option");
+    _tempEl.innerText = tab.name;
+    _tempEl.value = tab.id;
+    _tempEl.selected = tab.id == tabs.currentTabId;
+    tabselect.append(_tempEl);
+  }
+};
+//#endregion
+
+/**
+ * Function to initialize the Tab Selector TODO: May remove it...
+ */
+const initTabSelector = () => {
+  const tabselect = document.getElementById("tabselect");
+  // Add change listener
+  tabselect.addEventListener("change", function (ev) {
+    tabs.setCurrentTabId(this.value);
+  });
+  // Render
+  renderTabSelector();
+};
+
+/**
+ * Function to show "Open Directory" window
+ */
+const addItemDialog = () => {
+  currentWindow.hide();
+  electron.dialog
+    .showOpenDialog(undefined, {
+      properties: ["openDirectory"],
+      title: "Ordner hinzufügen",
+    })
+    .then((val) => {
+      if (val.filePaths.length > 0) {
+        tabs.addButtonToTab(val.filePaths[0]);
+      }
+      currentWindow.show();
+    });
+};
+
+//#region Helper functions
 
 const genInner = (p) => {
   if (typeof p == "string") {
@@ -25,80 +118,56 @@ const genInner = (p) => {
   }
 };
 
-const reRenderButtons = () => {
-  main.innerHTML = "";
-  tabs.getButtonsFromTab().forEach((link) => {
-    const newElParent = document.createElement("div");
-    const newEl = document.createElement("button");
-    newEl.innerText = genInner(link);
-    newEl.setAttribute("link", link);
+//#region Helper functions Coloring
+function getTextAndBackgroundColor(link) {
+  let background = "";
+  let textcolor = "";
 
-    const colors = getTextAndBackgroundColor(link);
+  const shade =
+    "#" + crypto.createHash("md5").update(link).digest("hex").substring(0, 6);
 
-    newEl.addEventListener("click", function () {
-      const link = this.getAttribute("link");
-      electron.shell.openExternal(link);
-    });
-    newEl.classList.add("button-inner");
-    newElParent.appendChild(newEl);
-    newElParent.classList.add("button-outer");
-    const newElRemove = document.createElement("button");
-    newElRemove.innerHTML = "&times;";
-    newElRemove.setAttribute("link", link);
-    newElRemove.addEventListener("click", function () {
-      const link = this.getAttribute("link");
-      tabs.removeButtonFromTab(link);
-    });
-    newElRemove.classList.add("button-remove");
+  background = shade;
+  textcolor = getTextColor(shade);
 
-    newElParent.style.backgroundColor = colors.background;
-    newEl.style.color = colors.textcolor;
+  return { background: background, textcolor: textcolor };
+}
 
-    newElParent.appendChild(newElRemove);
-    main.appendChild(newElParent);
-  });
-};
+function getRGB(c) {
+  return parseInt(c, 16) || c;
+}
 
-const renderTabSelector = () => {
-  const tabselect = document.getElementById("tabselect");
-  // Empty
-  tabselect.innerHTML = "";
-  // Add Buttons
-  for (let tab of tabs.getTabNamesAndIndexes()) {
-    const _tempEl = document.createElement("option");
-    _tempEl.innerText = tab.name;
-    _tempEl.value = tab.id;
-    _tempEl.selected = tab.id == tabs.currentTabId;
-    tabselect.append(_tempEl);
-  }
-};
+function getsRGB(c) {
+  return getRGB(c) / 255 <= 0.03928
+    ? getRGB(c) / 255 / 12.92
+    : Math.pow((getRGB(c) / 255 + 0.055) / 1.055, 2.4);
+}
 
-const initTabSelector = () => {
-  const tabselect = document.getElementById("tabselect");
-  // Add change listener
-  tabselect.addEventListener("change", function (ev) {
-    tabs.setCurrentTabId(this.value);
-  });
-  // Render
-  renderTabSelector();
-};
+function getLuminance(hexColor) {
+  return (
+    0.2126 * getsRGB(hexColor.substr(1, 2)) +
+    0.7152 * getsRGB(hexColor.substr(3, 2)) +
+    0.0722 * getsRGB(hexColor.substr(-2))
+  );
+}
 
-const addItemDialog = () => {
-  currentWindow.hide();
-  electron.dialog
-    .showOpenDialog(undefined, {
-      properties: ["openDirectory"],
-      title: "Ordner hinzufügen",
-    })
-    .then((val) => {
-      if (val.filePaths.length > 0) {
-        tabs.addButtonToTab(val.filePaths[0]);
-      }
-      currentWindow.show();
-    });
-};
+function getContrast(f, b) {
+  const L1 = getLuminance(f);
+  const L2 = getLuminance(b);
+  return (Math.max(L1, L2) + 0.05) / (Math.min(L1, L2) + 0.05);
+}
+
+function getTextColor(bgColor) {
+  const whiteContrast = getContrast(bgColor, "#ffffff");
+  const blackContrast = getContrast(bgColor, "#000000");
+
+  return whiteContrast > blackContrast ? "#ffffff" : "#000000";
+}
+//#endregion
+
+//#endregion
 
 const init = () => {
+  // Add Listeners for the menu
   document
     .getElementById("close")
     .addEventListener("click", () => currentWindow.close());
@@ -193,6 +262,7 @@ const init = () => {
     mainMenu.popup();
   });
 
+  // Add Listener for the add button
   document.getElementById("add").addEventListener("click", () => {
     addItemDialog();
   });
@@ -206,49 +276,3 @@ const init = () => {
 };
 
 init();
-
-function getTextAndBackgroundColor(link) {
-  let background = "";
-  let textcolor = "";
-
-  const shade =
-    "#" + crypto.createHash("md5").update(link).digest("hex").substring(0, 6);
-
-  background = shade;
-  textcolor = getTextColor(shade);
-
-  return { background: background, textcolor: textcolor };
-}
-
-// Geklauter Code:
-
-function getRGB(c) {
-  return parseInt(c, 16) || c;
-}
-
-function getsRGB(c) {
-  return getRGB(c) / 255 <= 0.03928
-    ? getRGB(c) / 255 / 12.92
-    : Math.pow((getRGB(c) / 255 + 0.055) / 1.055, 2.4);
-}
-
-function getLuminance(hexColor) {
-  return (
-    0.2126 * getsRGB(hexColor.substr(1, 2)) +
-    0.7152 * getsRGB(hexColor.substr(3, 2)) +
-    0.0722 * getsRGB(hexColor.substr(-2))
-  );
-}
-
-function getContrast(f, b) {
-  const L1 = getLuminance(f);
-  const L2 = getLuminance(b);
-  return (Math.max(L1, L2) + 0.05) / (Math.min(L1, L2) + 0.05);
-}
-
-function getTextColor(bgColor) {
-  const whiteContrast = getContrast(bgColor, "#ffffff");
-  const blackContrast = getContrast(bgColor, "#000000");
-
-  return whiteContrast > blackContrast ? "#ffffff" : "#000000";
-}
