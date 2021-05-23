49197d140f6c3195be518ad0c7b1df6932534055
markxoe <mark@oude-elberink.de>
2021-05-23 19:54:47 +0200

    :white_check_mark: Adding tests

---
+++ test/test.js
@@ -0,0 +1,37 @@
+const App = require("spectron").Application;
+const electronPath = require("electron");
+const path = require("path");
+
+const app = new App({ path: electronPath, args: [path.join(__dirname, "..")] });
+
+app
+  .start()
+  .then(function () {
+    return app.browserWindow.isVisible();
+  })
+  .then(function (isVisible) {
+    if (!isVisible) throw Error("Not Visible");
+  })
+  .then(function () {
+    return app.client.getTitle();
+  })
+  .then(function (title) {
+    if (title != "Explorer Switcher") throw Error("Wrong title ," + title);
+  })
+  .then(() => {
+    return app.client.auditAccessibility({}).then((audit) => {
+      if (audit.failed) {
+        throw Error(
+          audit.message + " " + (audit.results.length ? audit.results : "")
+        );
+      } else
+        console.log(audit.message, audit.results.length ? audit.results : "");
+    });
+  })
+  .catch(function (error) {
+    console.error("Test failed:", error);
+  })
+  .then(function () {
+    app.stop();
+    console.log("Test successful");
+  });
